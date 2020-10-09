44ad78d7dbc5cc7cb69ae58d59703660db79a275
Agunahwan Absin <vair.agun@gmail.com>
2020-10-10 06:58:15 +0700

    Initial Version

---
+++ Hello/Hello/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hello
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainPage = new GreetingsPage();
+            //InitializeComponent();
+
+            //MainPage = new ContentPage
+            //{
+            //    Content = new StackLayout
+            //    {
+            //        VerticalOptions = LayoutOptions.Center,
+            //        Children =
+            //        {
+            //            new Label
+            //            {
+            //                HorizontalTextAlignment=TextAlignment.Center,
+            //                Text="Welcome to Xamarin Forms!"
+            //            }
+            //        }
+            //    }
+            //};
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Hello/Hello/GreetingsPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hello
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetingsPage : ContentPage
+    {
+        public GreetingsPage()
+        {
+            Content = new Label
+            {
+                Text = "Greetings, Xamarin.Forms!"
+            };
+
+#if __IOS__
+            Padding = new Thickness(0, 20, 0, 0);
+#endif
+
+            Padding = GetPadding();
+        }
+
+        private static Thickness GetPadding()
+        {
+            switch (Device.RuntimePlatform)
+            {
+                case Device.Android:
+                    return new Thickness(5, 5, 5, 0);
+                default:
+                    return new Thickness(5, 5, 5, 0);
+            }
+        }
+    }
+}
